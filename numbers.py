8cd57ea81795bac3b45eb318dcaf36380178c3cc
dhavamani <59573808+dhavamani27@users.noreply.github.com>
2020-08-25 10:30:53 +0530

    created numbers.py
    This will print the all positive numbers in a range

---
+++ numbers.py
@@ -0,0 +1,8 @@
+list1=[12,-7,5,64,-14]
+print("Input:list1= ",list1)
+new_list=list(filter(lambda x:x>0,list1))
+print("Output: ",new_list)
+lis2=[12,14,-95,3]
+print("Input:list2= ",list2)
+new_list1=list(filter(lambda x:x>0,list2))
+print("Output: ",new-list1)
